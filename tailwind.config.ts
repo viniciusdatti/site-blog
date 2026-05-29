74a83342fb74498f76511e6504d1653d6ac9657d
Vinicius <vinicius@example.com>
2026-05-29 17:20:55 -0300

    Update project dependencies and restructure styles. Added autoprefixer, postcss, and tailwindcss to dependencies. Refactored index page layout and replaced global styles with Tailwind CSS directives.

---
+++ tailwind.config.ts
@@ -0,0 +1,15 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: 
+  ["./src/**/*.{js,ts,jsx,tsx}",
+  "./pages/**/*.{js,ts,jsx,tsx}",
+  "./components/**/*.{js,ts,jsx,tsx}",
+  "./app/**/*.{js,ts,jsx,tsx}",
+],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} satisfies Config;
+
