327e04d195b70914edf2494441c502be270ba9d2
Vinicius <vinicius@example.com>
2026-05-29 22:29:07 -0300

    chore: add contentlayer setup and blog post sources

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
+import { withContentlayer } from "next-contentlayer";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
 };
 
-export default nextConfig;
+export default withContentlayer(nextConfig);
