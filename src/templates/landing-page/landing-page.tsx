4053d7685f53c41e770f197ae9e42eeacbb93d71
Vinicius <vinicius@example.com>
2026-05-29 22:29:16 -0300

    feat: add landing page template and sections

---
+++ src/templates/landing-page/landing-page.tsx
@@ -0,0 +1,17 @@
+import {
+  CustomerStorySection,
+  FeatureSection,
+  HeroSection,
+  SupportSection,
+} from '@/templates/landing-page/sections';
+
+export const LandingPage = () => {
+  return (
+    <article className="flex flex-col gap-10 md:gap-20">
+      <HeroSection />
+      <FeatureSection />
+      <SupportSection />
+      <CustomerStorySection />
+    </article>
+  );
+};
